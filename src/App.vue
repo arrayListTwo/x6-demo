2e3d3da71fb6edb5755b120fa3fffb71b0eae977
平台中心-研发平台组-丁保磊 <1348909544@qq.com>
2025-07-09 15:31:35 +0800

    feat(AboutView): 添加流程图组件

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <div class="view-container">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+html, body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
 nav {
@@ -28,5 +46,10 @@ nav {
       color: #42b983;
     }
   }
+}
+
+.view-container {
+  flex: 1;
+  overflow: auto;
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,127 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-wrapper">
+    <div class="graph-wrapper">
+      <div class="app-content" id="container"></div>
+    </div>
+    <div class="btn-group">
+      <button @click="exportGraphData">导出</button>
+    </div>
   </div>
 </template>
+
+<script>
+import { Graph } from '@antv/x6'
+import { Snapline } from '@antv/x6-plugin-snapline'
+
+export default {
+  name: 'About',
+  data () {
+    return {
+      msg: 'This is an about page',
+      graph: null
+    }
+  },
+  mounted () {
+    const graphData = {
+      nodes: [
+        {
+          id: 'node1',
+          shape: 'rect',
+          x: 40,
+          y: 40,
+          width: 100,
+          height: 40,
+          label: 'hello node1',
+          attrs: {
+            // body 是选择器名称，选中的是 rect 元素
+            body: {
+              stroke: '#8f8f8f',
+              strokeWidth: 1,
+              fill: '#fff',
+              rx: 6,
+              ry: 6
+            }
+          }
+        },
+        {
+          id: 'node2',
+          shape: 'rect',
+          x: 160,
+          y: 180,
+          width: 100,
+          height: 40,
+          label: 'world node2',
+          attrs: {
+            body: {
+              stroke: '#8f8f8f',
+              strokeWidth: 1,
+              fill: '#fff',
+              rx: 6,
+              ry: 6
+            }
+          }
+        }
+      ],
+      edges: [
+        {
+          shape: 'edge',
+          source: 'node1',
+          target: 'node2',
+          label: 'x6',
+          attrs: {
+            // line 是选择器名称，选中的边的 path 元素
+            line: {
+              stroke: '#8f8f8f',
+              strokeWidth: 1
+            }
+          }
+        }
+      ]
+    }
+
+    this.graph = new Graph({
+      container: document.getElementById('container'),
+      width: 800,
+      height: 600,
+      background: {
+        color: '#F2F7FA'
+      }
+    })
+
+    // 添加 Snipline 插件
+    this.graph.use(new Snapline({
+      enabled: true
+    }))
+    // 渲染元素
+    this.graph.fromJSON(graphData)
+    // 局中显示
+    this.graph.centerContent()
+  },
+  methods: {
+    exportGraphData () {
+      console.log(JSON.stringify(this.graph.toJSON(), 2))
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.about-wrapper
+  display: flex
+  flex-direction: column
+  width: 100%
+  height: 100%
+  font-family: sans-serif
+
+  .graph-wrapper
+    display: flex
+    flex-direction: column
+    align-items: center
+
+    .app-content
+      height: 280px
+      border-radius: 5px
+      box-shadow: 0 12px 5px -10px rgb(0 0 0 / 10%), 0 0 4px 0 rgb(0 0 0 / 10%)
+  .btn-group
+    margin-top: 8px
+</style>
