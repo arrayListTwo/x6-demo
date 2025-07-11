e8d3613c6edaba4966673b0c809197cbd32bf539
平台中心-研发平台组-丁保磊 <1348909544@qq.com>
2025-07-11 17:24:31 +0800

    feat(AboutView): 实现自定义节点连接桩
    - 在 AboutView 组件中注册自定义节点类型 'custom-node-with-port'
    - 自定义节点包含顶部和底部的连接桩
    - 更新节点创建逻辑，使用新的自定义节点类型- 添加多个连接桩到节点中，分别属于顶部和底部组

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -64,6 +64,46 @@ export default {
       }
     })
 
+    // 自定义节点连接桩
+    Graph.registerNode('custom-node-with-port', {
+      inherit: 'rect',
+      width: 100,
+      height: 40,
+      attrs: {
+        body: {
+          stroke: '#8f8f8f',
+          strokeWidth: 1,
+          fill: '#fff',
+          rx: 6,
+          ry: 6
+        }
+      },
+      ports: {
+        groups: {
+          top: {
+            position: 'top',
+            attrs: {
+              circle: {
+                magnet: true,
+                stroke: '#e11616',
+                r: 5
+              }
+            }
+          },
+          bottom: {
+            position: 'bottom',
+            attrs: {
+              circle: {
+                magnet: true,
+                stroke: '#78dc1a',
+                r: 5
+              }
+            }
+          }
+        }
+      }
+    })
+
     /* const graphData = {
       nodes: [
         {
@@ -154,10 +194,30 @@ export default {
     }))
 
     const source = this.graph.addNode({
-      shape: 'custom-node',
+      shape: 'custom-node-with-port',
       x: 40,
       y: 40,
-      label: 'hello'
+      label: 'hello',
+      ports: {
+        items: [
+          {
+            id: 'port1',
+            group: 'top'
+          },
+          {
+            id: 'port2',
+            group: 'bottom'
+          },
+          {
+            id: 'port3',
+            group: 'top'
+          },
+          {
+            id: 'port4',
+            group: 'bottom'
+          }
+        ]
+      }
     })
 
     const target = this.graph.addNode({
